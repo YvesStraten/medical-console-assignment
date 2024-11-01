08f5ce7bfe50b59716ca4f8dd19b0cf3cfa844e6
YvesStraten <yves.straten@gmail.com>
2024-11-01 11:00:13 +0800

    Fixed sorting by name test failure

--- src/main/java/com/yvesstraten/medicalconsole/PatientsSortedByName.java
+++ src/main/java/com/yvesstraten/medicalconsole/PatientsSortedByName.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class PatientsSortedByName implements Comparator<Patient> {
 	@Override
 	public int compare(Patient o1, Patient o2) {
-		return o2.getName().compareToIgnoreCase(o1.getName());
+		return o1.getName().compareToIgnoreCase(o2.getName());
 	}
 }
