37d513ae5fa3edf9c4f70305db4a15708828fb42
YvesStraten <yves.straten@gmail.com>
2024-11-05 11:59:01 +0800

    Updated `Format`
    Bullet and enumerated lists now respect other lists

--- src/main/java/com/yvesstraten/medicalconsole/Format.java
+++ src/main/java/com/yvesstraten/medicalconsole/Format.java
@@ -1,21 +1,39 @@
 package com.yvesstraten.medicalconsole;
 
+import java.util.Arrays;
+
 public class Format {
   public static String enumeratedContent(String[] options) {
-		StringBuilder builder = new StringBuilder();
+    StringBuilder builder = new StringBuilder();
     for (int i = 0; i < options.length; i++) {
-      builder.append(String.format("[%d] %s \n", i + 1, options[i]));
+      if (!options[i].startsWith("-"))
+        builder.append(String.format("[%d] %s \n", i + 1, options[i]));
+			else 
+				builder.append(options[i]).append("\n");
     }
 
-		return builder.toString();
+    return builder.toString();
+  }
+
+  public static String enumeratedContent(String[] options, int startRange, int endRange) {
+    String[] sliced = Arrays.copyOfRange(options, startRange, endRange);
+    return enumeratedContent(sliced);
+  }
+
+  public static String enumeratedContent(String[] options, int startRange) {
+    String[] sliced = Arrays.copyOfRange(options, startRange, options.length);
+    return enumeratedContent(sliced);
   }
 
   public static String bulletedContent(String[] options) {
-		StringBuilder builder = new StringBuilder();
+    StringBuilder builder = new StringBuilder();
     for (int i = 0; i < options.length; i++) {
-      builder.append("- " + options[i]);
+			if(!options[i].startsWith("["))
+      	builder.append("- " + options[i]);
+			else 
+				builder.append(options[i]).append("\n");
     }
 
-		return builder.toString();
+    return builder.toString();
   }
 }
